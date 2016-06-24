ebd2d6a9ee9d9aba206c50660760575936f6c522
anhnt4 <anhnt4@smartosc.com>
2016-06-24 19:45:15 +0700

    Preview of Import

---
+++ app/code/local/Alex/Import/Model/Preview.php
@@ -0,0 +1,5 @@
+<?php
+
+class Alex_Import_Model_Preview extends Mage_Core_Model_Abstract {
+    
+}
--- app/code/local/Alex/Import/controllers/Adminhtml/ImportController.php
+++ app/code/local/Alex/Import/controllers/Adminhtml/ImportController.php
@@ -1,10 +1,13 @@
 <?php
 
+require_once 'app/code/core/Mage/ImportExport/controllers/Adminhtml/ImportController.php';
 class Alex_Import_Adminhtml_ImportController extends Mage_ImportExport_Adminhtml_ImportController
 {
     public function startPreviewAction()
     {
         $this->loadLayout('popup');
+        
+
         $this->renderLayout();
     }
 }
